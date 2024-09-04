f718919f2b666a30ac0dcd8861e8c6ab5ee42fb1
mscfeb <128896791+mscfeb@users.noreply.github.com>
2024-09-04 14:31:50 +0000

    variable declaration

--- 01-basics/01_variable.js
+++ 01-basics/01_variable.js
@@ -2,4 +2,5 @@ const accountId = 123456
 let accountName = "Saad"
 var accountCity = "chitapur"
 console.log([accountId, accountName, accountCity]);
+
 // var is also data type but not recomended to use it because it does not support block scope of a variable
