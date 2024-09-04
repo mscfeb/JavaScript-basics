e0b68f5953c84ca4df8c798d6a2bb5273cfa365b
mscfeb <128896791+mscfeb@users.noreply.github.com>
2024-09-04 14:53:02 +0000

    datatypes

---
+++ 01-basics/01_datatypes.js
@@ -0,0 +1,9 @@
+// number  : 2 to pwr of 53
+// bigint 
+// boolean :true/false
+// string : ""
+// null : standalone value
+// undefined : 
+// symbol : unique
+// objecct:
+console.log(typeof null);
