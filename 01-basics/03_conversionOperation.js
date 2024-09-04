30e28f9de26e895831259505826a9131be55d68c
mscfeb <128896791+mscfeb@users.noreply.github.com>
2024-09-04 16:52:46 +0000

    conversions

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = -1.5
+
+// let variable =Boolean(score)
+console.log(typeof score);
+console.log(score);
+
+
+// "33" => number ; number 33
+// null => Number; number 0
+// "" => number ; nuber 0
+// "33qs" => number ; number NaN(not a number)
+// NaN => Number; number NaN
+// undefined => number; number NaN
+// "isidjij" => number ; number NaN
+
+// 33 => string ; string "33"
+// NaN =>string; string NaN
+// Null =>string; string null
+// undefined => string; string undefined
+
+
+// "" => boolean; Boolean false
+// "dsds" => boolean; Boolean true
+// NaN, undefined, null => Boolean ; boolean false
